bdadf183e487ceba63bb90fef8a2674edc6aa428
Pham Quyen <paqvippro@gmail.com>
2025-06-04 16:22:08 +0700

    init

--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -3,11 +3,15 @@ package vn.hoidanit.laptopshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// @SpringBootApplication
+//include >< exclude
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(LaptopshopApplication.class, args);
+
 	}
 
 }
